990fb4a1cea891f5516d4ba596a0049b1717847d
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-05-28 11:43:24 -0700

    finished class livecode

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,5 +1,45 @@
 public class Traversal {
   public static void main(String[] args) {
+    TreeNode root = new TreeNode(10);
 
+    root.left = new TreeNode(9);
+    root.left.left = new TreeNode(5);
+    root.left.right = new TreeNode(2);
+
+    root.right = new TreeNode(15);
+    root.right.left = new TreeNode(-3);
+    root.right.right = new TreeNode(5);
+    root.right.right.right = new TreeNode(22);
+
+    preorder(root);
+    inorder(root);
+    postorder(root);
+  }
+
+  public static void preorder(TreeNode current) {
+    if(current == null) return;
+    System.out.println(current.data);
+
+    preorder(current.left);
+    preorder(current.right);
+  }
+
+  public static void inorder(TreeNode current) {
+    if(current == null) return;
+
+    inorder(current.left);
+
+    System.out.println(current.data);
+    
+    inorder(current.right);
+  }
+
+  public static void postorder(TreeNode current) {
+    if(current == null) return;
+
+    postorder(current.left);
+    postorder(current.right);
+
+    System.out.println(current.data);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,4 +1,22 @@
 public class TreeNode {
+ public int data;
+ public TreeNode left;
+ public TreeNode right;
+ 
+ public TreeNode(int data) {
+    this.data = data;
+ }
 
-  
+ public TreeNode (int data, TreeNode left) {
+    this.data = data;
+    this.left = left;
+ }
+
+ public TreeNode (int data, TreeNode left, TreeNode right) {
+    this.data = data;
+    this.left = left;
+    this.right = right;
+ }
+
+ //TreeNode(8, null, TreeNode(5))
 }
