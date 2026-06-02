d1e9ef2b919fcb1fbe51230b1a5f1345130072ae
tonymythical <174657016+tonymythical@users.noreply.github.com>
2026-06-02 11:00:20 -0700

    livecode pt2

--- src/Traversal.java
+++ src/Traversal.java
@@ -1,45 +1,70 @@
 public class Traversal {
   public static void main(String[] args) {
-    TreeNode root = new TreeNode(10);
+    TreeNode<Integer> root = new TreeNode<>(10);
 
-    root.left = new TreeNode(9);
-    root.left.left = new TreeNode(5);
-    root.left.right = new TreeNode(2);
+    root.left = new TreeNode<>(9);
+    root.left.left = new TreeNode<>(5);
+    root.left.right = new TreeNode<>(2);
 
-    root.right = new TreeNode(15);
-    root.right.left = new TreeNode(-3);
-    root.right.right = new TreeNode(5);
-    root.right.right.right = new TreeNode(22);
+    root.right = new TreeNode<>(15);
+    root.right.left = new TreeNode<>(-3);
+    root.right.right = new TreeNode<>(5);
+    root.right.right.right = new TreeNode<>(22);
 
-    preorder(root);
-    inorder(root);
+    TreeNode<String> stringRoot = new TreeNode<>("hello");
+
+
+    stringRoot.left = new TreeNode<>("e");
+    stringRoot.left.left = new TreeNode<>("f");
+    stringRoot.left.right = new TreeNode<>("g");
+
+    stringRoot.right = new TreeNode<>("h");
+    stringRoot.right.left = new TreeNode<>("i");
+    stringRoot.right.right = new TreeNode<>("f");
+    stringRoot.right.right.right = new TreeNode<>("j");
+
+    preorder(stringRoot);
+    // inorder(root);
     postorder(root);
+    printGreaterThan(root, 1);
   }
 
-  public static void preorder(TreeNode current) {
+  // Do a preorder traversal
+  // but ONLY PRINT the nodes that are greater than our threshold
+  public static void printGreaterThan(TreeNode<Integer> current, int threshold) {
+    if (current == null) return;
+    if (current.value > threshold) {
+      System.out.println(current.value);
+    }
+    printGreaterThan(current.left, threshold);
+    printGreaterThan(current.right, threshold);
+  }
+
+  public static void preorder(TreeNode<?> current) {
     if(current == null) return;
-    System.out.println(current.data);
+    System.out.println(current.value);
 
     preorder(current.left);
     preorder(current.right);
   }
 
-  public static void inorder(TreeNode current) {
+  public static void inorder(TreeNode<?> current) {
     if(current == null) return;
 
     inorder(current.left);
 
-    System.out.println(current.data);
+    System.out.println(current.value);
     
     inorder(current.right);
   }
 
-  public static void postorder(TreeNode current) {
+  public static <E> void postorder(TreeNode<E> current) {
     if(current == null) return;
 
     postorder(current.left);
     postorder(current.right);
 
-    System.out.println(current.data);
+    E myValue = current.value;
+    System.out.println(myValue);
   }
 }
--- src/TreeNode.java
+++ src/TreeNode.java
@@ -1,21 +1,17 @@
-public class TreeNode {
- public int data;
- public TreeNode left;
- public TreeNode right;
+public class TreeNode <T> {
+ public T value;
+ public TreeNode<T> left;
+ public TreeNode<T> right;
  
- public TreeNode(int data) {
-    this.data = data;
- }
 
- public TreeNode (int data, TreeNode left) {
-    this.data = data;
-    this.left = left;
- }
-
- public TreeNode (int data, TreeNode left, TreeNode right) {
-    this.data = data;
+ public TreeNode (T value, TreeNode<T> left, TreeNode<T> right) {
+    this.value = value;
     this.left = left;
     this.right = right;
+ }
+
+ public TreeNode(T value) {
+   this.value = value;
  }
 
  //TreeNode(8, null, TreeNode(5))
